edfc57935b222e35d933d5c2d61d22d0bb7ba8d9
Keitoran75 <huaili.z06@gmail.com>
2023-11-09 19:59:46 -0500

    Update 2D UNet

--- my_models.py
+++ my_models.py
@@ -692,11 +692,11 @@ class Conv1dEmbedder(nn.Module):
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, padding=1):
         super(ConvBlock, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, padding=padding)
-        self.bn1 = nn.BatchNorm1d(out_channels)
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, padding=padding)
+        self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv1d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, padding=padding)
-        self.bn2 = nn.BatchNorm1d(out_channels)
+        self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, padding=padding)
+        self.bn2 = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
         x = self.relu(self.bn1(self.conv1(x)))
@@ -705,22 +705,26 @@ class ConvBlock(nn.Module):
 
 
 class EncoderBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, use_pooling=True):
         super(EncoderBlock, self).__init__()
         self.conv_block = ConvBlock(in_channels=in_channels, out_channels=out_channels)
-        self.pool = nn.MaxPool1d(2)
+        self.use_pooling = use_pooling
+        if self.use_pooling:
+            self.pool = nn.MaxPool2d(2)
     
     def forward(self, x):
-        before_pool = self.conv_block(x)
-        x = self.pool(before_pool)
+        x = self.conv_block(x)
+        before_pool = x
+        if self.use_pooling:
+            x = self.pool(x)
         return x, before_pool
     
 
 class DecoderBlock(nn.Module):
     def __init__(self, in_channels, mid_channels, out_channels):
         super(DecoderBlock, self).__init__()
-        self.up = nn.ConvTranspose1d(in_channels=in_channels, out_channels=mid_channels, kernel_size=2, stride=2)
-        self.conv_block = ConvBlock(mid_channels+out_channels, out_channels)
+        self.up = nn.ConvTranspose2d(in_channels=in_channels, out_channels=mid_channels, kernel_size=2, stride=2)
+        self.conv_block = ConvBlock(mid_channels + out_channels, out_channels)
 
     def forward(self, x, skip):
         x = self.up(x)
@@ -728,43 +732,32 @@ class DecoderBlock(nn.Module):
         x = self.conv_block(x)
         return x
     
-class UNet1D(nn.Module):
+class UNet2D(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(UNet1D, self).__init__()
-        self.enc1 = EncoderBlock(in_channels, 64)
-        self.enc2 = EncoderBlock(64, 128)
-        self.enc3 = EncoderBlock(128, 256)
-        self.enc4 = EncoderBlock(256, 512)
-        self.bottleneck = ConvBlock(512, 1024)
-        self.dec4 = DecoderBlock(1024, 512, 512)
-        self.dec3 = DecoderBlock(512, 256, 256)
-        self.dec2 = DecoderBlock(256, 128, 128)
-        self.dec1 = DecoderBlock(128, 64, 64)
-        self.final = nn.Conv1d(64, out_channels, kernel_size=1)
-        self.fc = nn.Sequential(
-            nn.Linear(in_features=3072, out_features=192),
-            nn.BatchNorm1d(1),
-            nn.ReLU()
-        )
+        super(UNet2D, self).__init__()
+        self.enc1 = EncoderBlock(in_channels, 16, use_pooling=True)
+        self.enc2 = EncoderBlock(16, 32, use_pooling=True)
+
+        self.bottleneck = ConvBlock(32, 64)
+
+        self.dec2 = DecoderBlock(64, 32, 32)
+        self.dec1 = DecoderBlock(32, 16, 16)
+
+        self.final = nn.Conv2d(16, out_channels, kernel_size=1)
 
     def forward(self, x):
         x, skip1 = self.enc1(x)
         x, skip2 = self.enc2(x)
-        x, skip3 = self.enc3(x)
-        x, skip4 = self.enc4(x)
         x = self.bottleneck(x)
-        x = self.dec4(x, skip4)
-        x = self.dec3(x, skip3)
         x = self.dec2(x, skip2)
         x = self.dec1(x, skip1)
         x = self.final(x)
-        x = self.fc(x)
         return x
     
 if __name__ == "__main__":
-    model = UNet1D(in_channels=1, out_channels=1)
+    model = UNet2D(in_channels=3, out_channels=3)
     print(model)
 
-    input_tensor = torch.rand(1, 1, 3072)
+    input_tensor = torch.rand(10, 3, 8, 8)
     output = model(input_tensor)
     print(output.shape)
